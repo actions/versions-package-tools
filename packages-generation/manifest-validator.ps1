b10b90bd3c969f2f05f66e08c6a6d1184adaf2e4
Maxim Lobanov <v-malob@microsoft.com>
2020-06-16 11:21:35 +0300

    Update manifest-validator.ps1

--- packages-generation/manifest-validator.ps1
+++ packages-generation/manifest-validator.ps1
@@ -20,6 +20,7 @@ function Publish-Error {
 
 function Test-DownloadUrl {
     param([string] $DownloadUrl)
+    $DownloadUrl = $DownloadUrl + "w"
     $request = [System.Net.WebRequest]::Create($DownloadUrl)
     if ($AccessToken) {
         $request.Headers.Add("Authorization", $authorizationHeaderValue)
