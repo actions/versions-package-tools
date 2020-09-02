81039ddce33be9ac6a92473e594d2bc11913d54d
Nikita Bykov <v-nibyko@microsoft.com>
2020-09-02 15:52:51 +0300

    update

--- win-vs-env.psm1
+++ win-vs-env.psm1
@@ -3,8 +3,8 @@
 ###
 
 function Get-VSWhere {
-    $vswhere = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe";
-    
+    $vswhere = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+
     if (-not (Test-Path $vswhere )) {
         [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
         $vswhere = ".\vswhere.exe"
