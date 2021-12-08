adaa9700d761376b924470db334470b966db7b72
Maksim Shilov <maksim.shilov@akvelon.com>
2021-12-08 13:36:39 +0300

    Resolving comments

--- get-new-tool-versions/parsers/verify-added-to-image/verify-common-tool-parser.psm1
+++ get-new-tool-versions/parsers/verify-added-to-image/verify-common-tool-parser.psm1
@@ -15,11 +15,11 @@ function Search-ToolsVersionsNotOnImage {
             Select-Object -Unique
     }
     $toolsetUrl = "https://raw.githubusercontent.com/actions/virtual-environments/main/images/win/toolsets/toolset-2022.json"
-    $latestVersion = ((Invoke-RestMethod $toolsetUrl).toolcache |
-        Where-Object {$_.name -eq $ToolName -and $_.arch -eq $FilterArch}).versions |
+    $latestMinorVersion = (Invoke-RestMethod $toolsetUrl).toolcache |
+        Where-Object {$_.name -eq $ToolName -and $_.arch -eq $FilterArch} | 
+        ForEach-Object {$_.versions.Replace("*","0")} |
         Select-Object -Last 1
-    $latestMinorVesion = $latestVersion.Replace("*","0")
-    $versionsToAdd = $stableReleaseVersions | Where-Object {[version]$_ -gt [version]$latestMinorVesion}
+    $versionsToAdd = $stableReleaseVersions | Where-Object {[version]$_ -gt [version]$latestMinorVersion}
     
     return $versionsToAdd
 }
