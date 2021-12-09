5beca0b6a613bb6c13b9e8b2e8bf2b721775aadd
Maksim Shilov <maksim.shilov@akvelon.com>
2021-12-09 12:47:28 +0300

    Slightly optimized sorting

--- get-new-tool-versions/parsers/verify-added-to-image/verify-common-tool-parser.psm1
+++ get-new-tool-versions/parsers/verify-added-to-image/verify-common-tool-parser.psm1
@@ -7,13 +7,13 @@ function Search-ToolsVersionsNotOnImage {
     )
     
     $stableReleases = (Invoke-RestMethod $ReleasesUrl) | Where-Object stable -eq $true
-    if ($ToolName -eq "Node") {
-        $stableReleaseVersions = $stableReleases | ForEach-Object {$_.$FilterParameter.split(".")[0] + ".0"} |
-            Select-Object -Unique
-    } else {
-        $stableReleaseVersions = $stableReleases | ForEach-Object {$_.$FilterParameter.split(".")[0,1] -join"."} |
-            Select-Object -Unique
-    }
+    $stableReleaseVersions = $stableReleases | ForEach-Object {
+        if ($ToolName -eq "Node") { 
+            $_.$FilterParameter.split(".")[0] + ".0"
+        } else {
+            $_.$FilterParameter.split(".")[0,1] -join"."
+        }
+    } | Select-Object -Unique
     $toolsetUrl = "https://raw.githubusercontent.com/actions/virtual-environments/main/images/win/toolsets/toolset-2022.json"
     $latestMinorVersion = (Invoke-RestMethod $toolsetUrl).toolcache |
         Where-Object {$_.name -eq $ToolName -and $_.arch -eq $FilterArch} | 
