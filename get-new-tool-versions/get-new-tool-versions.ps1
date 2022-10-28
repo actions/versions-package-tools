4ca7ee36b43cc0e907d5c8fa562eb418f8fa478e
Aleksandr Chebotov <v-aleche@microsoft.com>
2022-10-28 18:01:23 +0200

    Replace set output

--- get-new-tool-versions/get-new-tool-versions.ps1
+++ get-new-tool-versions/get-new-tool-versions.ps1
@@ -21,8 +21,7 @@ $VersionsToBuild = $VersionsFromDist | Where-Object { $VersionsFromManifest -not
 if ($VersionsToBuild) {
     $availableVersions = $VersionsToBuild -join ", "
     Write-Host "The following versions are available to build:`n${availableVersions}"
-    Write-Host "::set-output name=TOOL_VERSIONS::${availableVersions}"
-    Write-Host "##vso[task.setvariable variable=TOOL_VERSIONS;isOutput=true]${availableVersions}"
+    "TOOL_VERSIONS=${availableVersions}" >> $env:GITHUB_OUTPUT
 } else {
     Write-Host "There aren't versions to build"
 }
