21da3ebcea8d6640b6cb88c5ae7baad2a36f76aa
Haritha <73516759+HarithaVattikuti@users.noreply.github.com>
2026-06-04 13:07:58 -0500

    Update version inclusion logic for Node.js parser

--- get-new-tool-versions/parsers/node-parser.psm1
+++ get-new-tool-versions/parsers/node-parser.psm1
@@ -22,9 +22,16 @@ class NodeVersionsParser: BaseVersionsParser {
         if ($Version.Major -lt 8) {
             return $false
         }
-
-        # For Node.JS, we should include all LTS versions (all even-numbered releases)
-        # https://nodejs.org/en/about/releases/
-        return $Version.Major % 2 -eq 0
+        elseif ($Version.Major -lt 27)
+        {
+            # For Node.JS, we should include all LTS versions (all even-numbered releases)
+            # https://nodejs.org/en/about/releases/
+            return $Version.Major % 2 -eq 0
+        }
+        else
+        {
+            # https://nodejs.org/en/blog/announcements/evolving-the-nodejs-release-schedule
+            return $true
+        }
     }
 }
